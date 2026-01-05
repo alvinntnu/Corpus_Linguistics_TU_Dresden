b929009fe9b27e95803b5fe5e9206e88150b1190
Alvin Chen <alvinchen@Alvins-Mac-Studio-3.local>
2026-01-05 08:23:45 +0800

    allfiles

---
+++ install.R
@@ -0,0 +1,8 @@
+install.packages("showtext")
+install.packages("tidyverse")
+install.packages("tidytext")
+install.packages("readtext")
+install.packages("quanteda")
+install.packages("jiebaR")
+install.packages("rmarkdown")
+install.packages("prettydoc")
