eaac6f3e4ea78d7e77eb82f623693e28a0cac424
alvinntnu <alvinworks@gmail.com>
2026-01-10 08:37:17 +0100

    all

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 - This is a hands-on tutorial prepared for the workshop series at TU Dresden (Jan, 2025).
 - We assume no coding background although the amount of codeing at the first sight can be intimidating.
 - Topics include:
+  -  What is Corpus Linguistics? [View Tutorial (HTML)](https://alvinntnu.github.io/Corpus_Linguistics_TU_Dresden/what_is_CL.html)]
   -  R Fundamentals [View Tutorial (HTML)](https://alvinntnu.github.io/Corpus_Linguistics_TU_Dresden/r-fundamentals.html)
   -  Corpus Linguistics: An Introduction [View Tutorial (HTML)](https://alvinntnu.github.io/Corpus_Linguistics_TU_Dresden/Corpus_Ling_Intro_ENC2036.html)
   -  Collostruction Analysis [View Tutorial (HTML)](https://alvinntnu.github.io/Corpus_Linguistics_TU_Dresden/Collostruction_Analysis.html)
--- knitting_html.R
+++ knitting_html.R
@@ -1,6 +1,7 @@
 ## knit all htmls
 
 library(prettydoc)
+rmarkdown::render("what_is_CL.Rmd", html_pretty(theme="leonids", highlight="github", toc=T, number_sections=T, df_print="paged",css="static/style.css"), output_dir = "docs")
 rmarkdown::render("r-fundamentals.Rmd", html_pretty(theme="leonids", highlight="github", toc=T, number_sections=T, df_print="paged",css="static/style.css"), output_dir = "docs")
 rmarkdown::render("Corpus_Ling_Intro_ENC2036.Rmd", html_pretty(theme="leonids", highlight="github", toc=T, number_sections=T, df_print="paged",css="static/style.css"), output_dir = "docs")
 rmarkdown::render("collostruction_analysis.Rmd", html_pretty(theme="leonids", highlight="github", toc=T, number_sections=T, df_print="paged",css="static/style.css"), output_dir = "docs")
